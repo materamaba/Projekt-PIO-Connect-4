2b63f3e916ab13c8600375d35ac2a491900d48d9
Patryk Jędraszek <jedraszekpatryk@onet.pl>
2026-06-05 19:14:42 +0200

    Utworzenie testow dla klasy Krążek

---
+++ app/src/main/java/org/projekt/Rozgrywka.java
@@ -0,0 +1,5 @@
+package org.projekt;
+
+public class Rozgrywka {
+    
+}
---
+++ app/src/test/java/org/projekt/RozgrywkaTest.java
@@ -0,0 +1,8 @@
+package org.projekt;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RozgrywkaTest {
+
+}
