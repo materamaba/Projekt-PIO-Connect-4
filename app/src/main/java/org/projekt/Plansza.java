42896882821036a16f36aedc17ace1134b5d1f25
Patryk Jędraszek <jedraszekpatryk@onet.pl>
2026-06-05 20:32:56 +0200

    Utworzenie klasy Plansza

--- app/src/main/java/org/projekt/Plansza.java
+++ app/src/main/java/org/projekt/Plansza.java
@@ -1,5 +1,29 @@
 package org.projekt;
 
 public class Plansza {
+    private Krażek[][] grid = new Krażek[6][7];
 
+    public void insert(int col, int playerID) throws Exception{
+        if(col < 0 || col > 6){
+            throw new Exception("Invalid column");
+        }
+        int i = 5;
+        int flag = 0;
+        while (i > -1) {
+            if(grid[i][col] == null){
+                grid[i][col] = new Krażek(playerID);
+                flag = 1;
+                break;
+            }
+            i--;
+        }
+        if(flag == 0){
+            throw new Exception("Column is full");
+        }
+
+    }
+
+    public Krażek getDisc(int row,int col){
+        return grid[row][col];
+    }
 }
