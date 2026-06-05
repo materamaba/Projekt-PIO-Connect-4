529b713030ceff7ddb8220ebfd70785635c0112a
Patryk Jędraszek <jedraszekpatryk@onet.pl>
2026-06-05 19:20:38 +0200

    Utworzenie klasy Krążek

--- app/src/main/java/org/projekt/Krażek.java
+++ app/src/main/java/org/projekt/Krażek.java
@@ -1,5 +1,15 @@
 package org.projekt;
 
 public class Krażek {
+        private int PlayerId;
+    public Krażek(int PlayerId) throws Exception{
+        if(PlayerId != 1 && PlayerId != 2){
+             throw new Exception("Incorrect PlayerID");
+        }
+        this.PlayerId = PlayerId;
+    }
 
+    public int GetPlayerId(){
+        return PlayerId;
+    }
 }
