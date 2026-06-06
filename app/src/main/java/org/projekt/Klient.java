e60198904cc5cce44bab12892c419f5b751498a7
Mateusz Gniatkowski <255590@edu.p.lodz.pl>
2026-06-06 17:19:21 +0200

    Napisanie testu connectToBadPortTest() i dodanie metody do klienta

--- app/src/main/java/org/projekt/Klient.java
+++ app/src/main/java/org/projekt/Klient.java
@@ -5,7 +5,21 @@ import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class Klient extends Application {
+    private Socket socket;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+
+    public void connectToServer(String ipAddress, int port) throws Exception {
+        if(port < 0 || port > 65535){
+            throw new Exception("Bad port");
+        }
+    }
 
     @Override
     public void start(Stage primaryStage) {
--- app/src/test/java/org/projekt/KlientTest.java
+++ app/src/test/java/org/projekt/KlientTest.java
@@ -1,4 +1,18 @@
 package org.projekt;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class KlientTest {
+    @Test
+    void connectToBadPortTest() {
+        try{
+            Klient client = new Klient();
+            client.connectToServer("localhost", -1234);
+            fail("Client should check port");
+        }catch(Exception e){
+            // this test should throw exception
+        }
+    }
 }
