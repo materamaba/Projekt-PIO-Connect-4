a6660cf634baecc65f99abdca17459f2e3be428f
Mateusz Gniatkowski <255590@edu.p.lodz.pl>
2026-06-07 11:16:38 +0200

    Dodanie metod do komunikacji z serwerem na Kliencie

--- app/src/main/java/org/projekt/Klient.java
+++ app/src/main/java/org/projekt/Klient.java
@@ -1,6 +1,7 @@
 package org.projekt;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -9,8 +10,10 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.sql.ClientInfoStatus;
 
 public class Klient extends Application {
+    private Rozgrywka gameToDisplay;
     private Socket socket;
     private ObjectInputStream in;
     private ObjectOutputStream out;
@@ -24,16 +27,44 @@ public class Klient extends Application {
             socket = new Socket(ipAddress, port);
 
             out = new ObjectOutputStream(socket.getOutputStream());
-            out.flush();
-
             in = new ObjectInputStream(socket.getInputStream());
         } catch (IOException e) {
-            throw new Exception("Cant connect to server");
+            throw new Exception("Can't connect to server");
         }
+    }
+
+    private void waitForServerToStartGame(){
+        try{
+            in.readObject();
+        }catch(Exception e){
+            System.out.println("Server disconnected");
+        }
+    }
+
+    private void startClientAndServerCommunicationThread(){
+        new Thread(() -> {
+            try{
+                gameToDisplay = (Rozgrywka) in.readObject();
+            }catch(Exception e){
+                System.out.println("Server disconnected");
+            }
+        }).start();
+    }
+
+    private void startGameDisplayThread(){
+        new Thread(() -> {
+
+        }).start();
     }
 
     @Override
     public void start(Stage primaryStage) {
+        primaryStage.setOnCloseRequest(event -> {
+            System.out.println("Game window closed");
+            Platform.exit();
+            System.exit(0);
+        });
+
         StackPane stackPane = new StackPane();
         Scene scene = new Scene(stackPane, 300, 250);
 
@@ -41,7 +72,21 @@ public class Klient extends Application {
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Klient client = new Klient();
+
+        try {
+            client.connectToServer("localhost", 1234);
+        }catch(Exception e){
+            System.out.println("Cant connect to server");
+            System.exit(0);
+        }
+
+        client.waitForServerToStartGame();
+
+        client.startClientAndServerCommunicationThread();
+        client.startGameDisplayThread();
+
         launch(args);
     }
 }
